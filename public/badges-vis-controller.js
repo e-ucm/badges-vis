fafa9b2795be56f1e68bcd57cc0ee8023486bd50
RotaruDan <drotaru@ucm.es>
2017-11-30 18:32:10 +0100

    Core visualization files

---
+++ public/badges-vis-controller.js
@@ -0,0 +1,47 @@
+'use strict';
+
+//import uiModules from 'ui/modules';
+import {uiModules} from 'ui/modules';
+import {AggResponseTabifyProvider} from 'ui/agg_response/tabify/tabify';
+
+// get the kibana/badges-vis module, and make sure that it requires the "kibana" module if it didn't already
+const module = uiModules.get('kibana/badges-vis', ['kibana']);
+
+const numeral = require('numeral');
+
+module.controller('BadgesVisController', function ($scope, $element, Private) {
+
+  var percentNiceAggResponse = Private(require('./lib/agg_response'));
+
+  var computeValue = function _render(results) {
+
+    if (results.length !== 2) {
+      return;
+    }
+
+    var val1 = results[0].value;
+    var val2 = results[1].value;
+
+    if (val1 === 0) {
+      return;
+    }
+
+    var value;
+    if ($scope.vis.params.division) {
+      value = val2 / val1;
+    } else {
+      value = val2;
+    }
+
+    var valueFormatted = results[0].formatNumber ? numeral(value).format(results[0].formatNumber) : value;
+    $scope.vis.params.value = valueFormatted;
+  };
+
+  $scope.$watch('esResponse', function (resp) {
+    if (resp) {
+      var chartData = percentNiceAggResponse($scope.vis, resp);
+      computeValue(chartData);
+    }
+  });
+});
+
---
+++ public/badges-vis.js
@@ -0,0 +1,54 @@
+import 'plugins/badges-vis/badges-vis.less';
+import 'plugins/badges-vis/badges-vis-controller';
+import {VisSchemasProvider} from 'ui/vis/schemas';
+import badgesVisTemplate from 'plugins/badges-vis/badges-vis.html';
+import badgesVisParamsTemplate from 'plugins/badges-vis/badges-vis-params.html';
+import {VisVisTypeProvider} from 'ui/vis/vis_type';
+import {TemplateVisTypeProvider} from 'ui/template_vis_type/template_vis_type';
+import {VisTypesRegistryProvider} from 'ui/registry/vis_types';
+// register the provider with the visTypes registry
+VisTypesRegistryProvider.register(badgesVisProvider);
+
+function badgesVisProvider(Private) {
+  const TemplateVisType = Private(TemplateVisTypeProvider);
+  const Schemas = Private(VisSchemasProvider);
+  const VisType = Private(VisVisTypeProvider);
+
+  return new TemplateVisType({
+    name: 'badges-vis',
+    title: 'Badges and achievements widget',
+    icon: 'fa-table',
+    description: 'This is Kibana 5 plugin which uses the JavaScript to display badges and achievements.',
+    category: VisType.CATEGORY.OTHER,
+    template: badgesVisTemplate,
+    params: {
+      defaults: {
+        imageUrl: 'https://github.com/e-ucm/badges-vis/raw/master/images/badges/trophy_acchievement.png',
+        text: 'Badge text',
+        value: '0',
+        division: true,
+        numeralFormat: '[{ "numeralFormat" : "%00,0" }]'
+      },
+      editor: badgesVisParamsTemplate
+    },
+    schemas: new Schemas([
+      {
+        group: 'metrics',
+        name: 'metric',
+        title: 'Count of',
+        min: 1,
+        max: 5,
+        defaults: [{type: 'count', schema: 'metric'}]
+      },
+      {
+        group: 'buckets',
+        name: 'buckets',
+        title: 'Divided by',
+        min: 1,
+        max: 1,
+        aggFilter: ['!geohash_grid'],
+        defaults: [{type: 'filters'}]
+      }
+    ])
+  });
+}
